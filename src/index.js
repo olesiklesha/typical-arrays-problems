1346846f3992710312e2a8847b11df5bd3387d2d
Alex Olesik <olesik736@gmail.com>
2021-03-04 20:10:23 +0300

    feat: write a functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array == undefined || array.length == 0) return 0;
+
+	let compare = function (a, b) {
+		if (a > b) return 1;
+		if (a == 0) return 0;
+		if (a < b) return -1;
+	}
+
+	array.sort(compare);
+
+	return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array == undefined || array.length == 0) return 0;
+
+	let compare = function (a, b) {
+		if (a < b) return 1;
+		if (a == 0) return 0;
+		if (a > b) return -1;
+	}
+
+	array.sort(compare);
+
+	return array[0];
+	return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array == undefined || array.length == 0) return 0;
+
+	let result = (array.reduce((sum, current) => sum + current, 0)) / array.length;
+	return result;
 }
